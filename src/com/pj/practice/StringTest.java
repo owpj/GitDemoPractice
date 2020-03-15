3ff105c4c134a523f003140eab5d0fcc80756c23
JAY <JAY@DESKTOP-98U7809>
2020-03-15 22:56:01 +0530

    First commit

---
+++ src/com/pj/practice/StringTest.java
@@ -0,0 +1,15 @@
+package com.pj.practice;
+
+public class StringTest {
+	
+	public static void main(String[] args) {
+		
+		String s="Jay";
+		String s1 ="Jay";
+		System.out.println(s==s1);
+		System.out.println(s.equals(s1));
+		
+		
+	}
+
+}
