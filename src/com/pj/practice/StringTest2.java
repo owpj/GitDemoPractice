94db902a8cb61ecfb71224dde989fecc969de2f7
JAY <JAY@DESKTOP-98U7809>
2020-03-16 01:24:47 +0530

    commiting StringTest2 to branchR

---
+++ src/com/pj/practice/StringTest2.java
@@ -0,0 +1,16 @@
+package com.pj.practice;
+
+public class StringTest2 {
+	
+	public static void main(String[] args) {
+		
+		String s="Jay";
+		String s1 ="Jay";
+		System.out.println(s==s1);
+		System.out.println(s.equals(s1));
+		System.out.println("String concept   ........!");
+		
+		
+	}
+
+}
